0e76416d53c663fb8bbd1f8f1ffc5676e81eea48
david.aristizabal <david.aristizabal@kiplan.io>
2024-09-01 17:16:55 -0500

    added mongo db in other router

--- fastapi/db/models/user.py
+++ fastapi/db/models/user.py
@@ -1,7 +1,8 @@
+from typing import Optional
 from pydantic import BaseModel
 
 # Entidad user
 class User(BaseModel):
-  id: int
+  id: Optional[str] = None
   username: str
   email: str
--- fastapi/routers/users_db.py
+++ fastapi/routers/users_db.py
@@ -1,73 +1,65 @@
 from fastapi import APIRouter, HTTPException, status
 from db.models.user import User
 from db.client import db_client
+from db.schemas.user import user_schema, users_schema
+from bson import ObjectId
 
 router = APIRouter(tags=["users", "mongo"],
                    prefix="/user_db",
                    responses={status.HTTP_404_NOT_FOUND: {"message": "No encontrado"}})
 
-users_list = []
+def search_user(field: str, key):
+  try:
+    user = db_client.users.find_one({field: key})
+    return User(**user_schema(user))
+  except:
+    return { "error": "No se ha encontrado el usuario"}
 
-@router.get("/users")
+@router.get("/", response_model=list[User])
 async def users():
-  return users_list
+  return users_schema(db_client.users.find())
 
-@router.get('/{id}')
-async def user(id: int):
-    user = filter(lambda user: user.id == id, users_list)
-    try:
-       return list(user)[0]
-    except:
-       return {"error": "No se ha encontrado al usuario"}
-    
-@router.get('/userquery')
-async def user(id: int):
-    user = filter(lambda user: user.id == id, users_list)
-    try:
-       return list(user)[0]
-    except:
-       return { "error": "No se ha encontrado el usuario"}
+@router.get('/user/{id}') # Path
+async def user(id: str):
+    print("Inside user_db/{id}")
+    return search_user("_id", ObjectId(id))
+     
+@router.get('/userquery') #Query
+async def user(id: str):
+    print("Inside userquery route")
+    return search_user("_id", ObjectId(id))
 
 @router.post('/', response_model=User, status_code=status.HTTP_201_CREATED)
 async def user(user: User):
-    if type(search_user(user.id)) == User:
-      raise HTTPException(status_code=status.HTTP_204_NO_CONTENT, detail="El usuario ya existe")
-    else:
-      users_list.append(user)
-      return user
-
-def search_user(id: int):
-    user = filter(lambda user: user.id == id, users_list)
-    try:
-      return list(user)[0]
-    except:
-      return { "error": "No se ha encontrado el usuario"}
+    
+    if type(search_user("email", user.email)) == User:
+      raise HTTPException(status_code=status.HTTP_302_FOUND,
+                          detail="El usuario ya existe")
+    
+    user_dict = dict(user)
+    del user_dict["id"]    
+    
+    id = db_client.users.insert_one(user_dict).inserted_id
+    
+    new_user = user_schema(db_client.users.find_one({"_id": id}))
+      
+    return User(**new_user)
 
 @router.put('/')
 async def user(user: User):
-  found = False
+  user_dict = dict(user)
+    
+  try:
+    db_client.users.find_one_and_replace({"_id": ObjectId(user.id)}, user_dict)
+  except:
+    return { "error": "No se ha actualizado al usuario"}
+  
+  return search_user("_id", ObjectId(user.id))
 
-  for index, saved_user in enumerate(users_list):
-    if saved_user.id == user.id:
-      users_list[index] = user
-      found = True
-
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def user(id: str):
+  found = db_client.users.find_one_and_delete({"_id": ObjectId(id)})
+  
   if not found:
     return { "error": "No se ha encontrado el usuario"}
-  else:
-     return user
-
-@router.delete("/{id}")
-async def user(id: int):
-  found = False
-
-  for index, user in enumerate(users_list):
-    if user.id == id:
-      user_index = index
-      found = True
-
-  if found:
-    return users_list.pop(user_index)
-  else:
-    return { "error": "No existe ningún usuario con ese ID"}
       
