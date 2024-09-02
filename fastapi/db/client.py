cf361888fb40e5862ec0fff896305f7a7bb2dd94
david.aristizabal <david.aristizabal@kiplan.io>
2024-09-01 20:58:19 -0500

    New conection to cloud server

--- fastapi/db/client.py
+++ fastapi/db/client.py
@@ -1,3 +1,16 @@
-from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-db_client = MongoClient().local
+# Base de datos local
+# db_client = MongoClient().local
+
+# Base de datos remota
+URI = "mongodb+srv://admin:admin@cursopython.bdb63.mongodb.net/?retryWrites=true&w=majority&appName=CursoPython"
+client = MongoClient(URI, server_api=ServerApi('1'))
+
+try:
+  client.admin.command('ping')
+  print("Connection with MongoDB client success!!!")
+  db_client = client["CursoPython"]
+except Exception as e:
+  print(e)
