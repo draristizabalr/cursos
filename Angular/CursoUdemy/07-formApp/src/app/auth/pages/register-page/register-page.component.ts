7897b5c75b6d0c78760fe6e23e474073e310b35c
david.aristizabal <david.aristizabal@kiplan.io>
2024-11-06 23:26:39 -0500

    update 06/11/2024 23:26

--- Angular/CursoUdemy/07-formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ Angular/CursoUdemy/07-formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
@@ -12,5 +13,23 @@ import { Component } from '@angular/core';
   `
 })
 export class RegisterPageComponent {
+
+  private fb: FormBuilder = new FormBuilder()
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required]],
+    email: ['', [ Validators.required]],
+    username: ['', [ Validators.required]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required]]
+  })
+
+  onSave(): void {
+    this.myForm.markAllAsTouched()
+  }
+
+  isValidField( field: string ) {
+    // TODO: obtener desde un servicio
+  }
 
 }
--- Angular/CursoUdemy/07-formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ Angular/CursoUdemy/07-formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+interface Person {
+  gender: 'M' | 'F';
+  wantNotifications: boolean;
+}
 
 @Component({
   templateUrl: './switches-page.component.html',
@@ -6,4 +12,31 @@ import { Component } from '@angular/core';
 })
 export class SwitchesPageComponent {
 
+  private fb: FormBuilder = new FormBuilder()
+
+  public myForm: FormGroup = this.fb.group({
+    gender: [ 'M', Validators.required ],
+    wantNotifications: [ true, Validators.required ],
+    termsAndConditions: [ false, Validators.requiredTrue ]
+  })
+
+  public person?: Person
+
+  onSave(): void {
+    if( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      alert('Debes aceptar los términos y condiciones')
+      return
+    }
+
+    const { termsAndConditions, ...newPerson } = this.myForm.value
+
+    this.person = newPerson
+
+    this.myForm.reset()
+  }
+
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched
+  }
 }
