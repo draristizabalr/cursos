0b5dc8f3ab9d5aa419f4c2c9ef4c41cf7d921317
draristizabalr <draristizabalr@unal.edu.co>
2025-06-10 08:39:01 -0500

    Start new course Angular pro

--- Angular/AngularPro/02-pokemon-ssr/src/app/pages/contact-page/contact-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-contact-page',
-  imports: [],
-  templateUrl: './contact-page.component.html',
-})
-export class ContactPageComponent { }
--- Angular/AngularPro/02-pokemon-ssr/src/app/pages/pricing-page/pricing-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-pricing-page',
-  imports: [],
-  templateUrl: './pricing-page.component.html',
-})
-export class PricingPageComponent { }
